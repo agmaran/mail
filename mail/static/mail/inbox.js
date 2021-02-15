382ed97b2ccb339223920938971167b9b78f89be
ArantxaGarciaMoreno <arantxagarciamoreno@gmail.com>
2021-02-15 12:50:35 -0400

    Send Mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,10 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // Send mail
+
+  document.querySelector('#compose-form').addEventListener('submit', send_mail);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -20,10 +24,13 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  // Hide error message div
+  document.querySelector('#error').style.display = 'none';
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -31,3 +38,31 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
+function send_mail(event) {
+
+  event.preventDefault();
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body,
+    }),
+  })
+    .then(response => response.json())
+    .then(result => {
+      if (result.error) {
+        document.querySelector('#error').innerHTML = result.error;
+        document.querySelector('#error').style.display = 'block';
+      } else {
+        load_mailbox('sent');
+      }
+    })
+}
+
+
